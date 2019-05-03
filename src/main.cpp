a4729e8edb1d0731eb955d661438ff686e40dabe
JBas <joshua.n.bas@gmail.com>
2019-05-03 17:17:32 -0400

    made changes to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,35 @@
 APES robot;
 static void sigint_handler(int sig);
 
-int main() {
-    int isConnected;
+int checkConnection() {
 
+}
+
+int main() {
     signal(SIGINT, sigint_handler);
 
     if (robot.setup() == -1) {
+        fprintf(stderr, "ERROR: APES system setup failure!");
+        fprintf(stderr, " Shutting down!\n");
         robot.finish();
     }
-    while(isConnected)
+
+
+
+
+
+
+    while(checkConnection())
     {
     	int data = robot.measMCP3008(0, 7);
     	printf("adc val = %d\n", data);
     	//robot.measWOB();
     }
 
-    fprintf(stderr, "ERROR: APES system disconnected! Shutting down!\n");
+    fprintf(stderr, "ERROR: APES system disconnected!");
+    fprintf(stderr, " Shutting down!\n");
     robot.finish()
-    return 0;
+    return -1;
 }
 
 static void sigint_handler(int sig) {
