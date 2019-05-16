ea0d571db197b46de8a868a43649d354d5352ef0
JBas <joshua.n.bas@gmail.com>
2019-05-16 00:01:18 -0400

    refactored components into one file

--- src/components.cpp
+++ src/components.cpp
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+namespace py = pybind11;
+using namespace py::literals;
+
 /*
     HELPER FUNCTIONS
 */
@@ -58,7 +61,7 @@ float D_temp(therm_t thermo) {
     return fTemp - thermo->iTemp;
 }
 
-void freeTherm(therm_t thermo) {
+void free_therm(therm_t thermo) {
     free(thermo);
 }
 
@@ -89,7 +92,7 @@ float read_curr(amm_t ammeter) {
     return (float)readADC(bus, channel);
 }
 
-void freeAmm(amm_t ammeter) {
+void free_amm(amm_t ammeter) {
     free(ammeter);
 }
 
@@ -145,6 +148,49 @@ int read_level(level_t wlevel) {
     return level;
 }
 
-void freeLevel(level_t wlevel) {
+void free_level(level_t wlevel) {
     free(wlevel);
+}
+
+
+/*
+    WOB FUNCTIONS
+*/
+wob_t make_wob() {
+    // this assumes the pybind interpreter has been initialized
+    // in  APES::setup()!
+    wob_t loadcell = calloc(1, sizeof(wob));
+    assert(loadcell != NULL);
+
+    py::object hx711 = py::module::import("libraries/hx711").attr("HX711");
+    assert(hx711 != NULL);
+
+    loadcell->HX711 = hx711(5, 6);
+    loadcell->HX711.attr("set_reading_format")("byte_format" _a="MSB", "bit_format" _a="MSB");
+    loadcell->HX711.attr("set_reference_unit")(1);
+    loadcell->HX711.attr("reset")();
+    loadcell->HX711.attr("tare")();
+
+    return loadcell;
+}
+
+float read_wob(wob_t loadcell) {
+    py::object value;
+    value = loadcell->HX711.attr("get_weight")("times" _a=5);
+    loadcell->HX711.attr("power_down")();
+    loadcell->HX711.attr("power_up")();
+
+    //@TODO: look up how to convert pybind value to C++ primitive
+
+    return 1;
+}
+
+void free_wob(wob_t loadcell) {
+    // this assumes the pybind interpreter will be
+    // finalized in APES::finish()
+    if (loadcell->HX711 != NULL) {
+        loadcell->HX711.attr("finish")();
+        loadcell->HX711.release();
+    }
+    return;
 }
--- src/components.h
+++ src/components.h
@@ -1,6 +1,8 @@
 #ifndef __COMPONENTS_H__
 #define __COMPONENTS_H__
 
+#include "include/pybind11/embed.h"
+
 typedef struct therm {
     int bus_addr;
     float max_T;
@@ -12,7 +14,7 @@ typedef therm* therm_t;
 therm_t make_therm(int bus_addr, float max_T);
 float read_temp(therm_t thermo);
 float D_temp(therm_t thermo);
-void freeTherm(therm_t thermo);
+void free_therm(therm_t thermo);
 
 
 typedef struct amm {
@@ -24,7 +26,7 @@ typedef amm* amm_t;
 
 amm_t make_amm(int bus_addr, float max_I);
 float read_curr(amm_t ammeter);
-void freeAmm(amm_t ammeter);
+void free_amm(amm_t ammeter);
 
 
 typedef struct level {
@@ -36,6 +38,16 @@ typedef level* level_t;
 
 level_t make_level(int bus_start, int bus_end, int sample_freq);
 int read_level(level_t wlevel);
-void freeLevel(level_t wlevel);
+void free_level(level_t wlevel);
+
+typedef struct wob {
+    pybind11::object HX711;
+} wob;
+
+typedef wob* wob_t;
+
+wob_t make_wob();
+float read_wob(wob_t loadcell);
+void free_wob(wob_t loadcell);
 
 #endif
