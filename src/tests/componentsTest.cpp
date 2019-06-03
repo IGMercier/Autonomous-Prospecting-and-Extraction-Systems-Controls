4e00160af247d4bce87523c7c0150cc41e5f27c1
JBas <joshua.n.bas@gmail.com>
2019-06-02 21:10:22 -0400

    pushing for testing

--- src/tests/componentsTest.cpp
+++ src/tests/componentsTest.cpp
@@ -14,9 +14,9 @@ int main(int argc, char **argv) {
     */
 
     Stepper* stepper = new Stepper(36, 35);
-    stepper->stepper_drive(1, 50);
+    stepper->stepper_drive(1, 50, 1000);
 
-    sleep(1);
+    sleep(5);
     
     stepper->stepper_stop();
 
